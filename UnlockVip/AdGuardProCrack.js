46378a57f05440f6667e7d03ab1fb7da0e54738e
Jason <129001357+Jasonaax@users.noreply.github.com>
2024-07-03 11:07:00 +0800

    Update AdGuardProCrack.js

--- UnlockVip/AdGuardProCrack.js
+++ UnlockVip/AdGuardProCrack.js
@@ -16,7 +16,7 @@
 
 [rewrite_local]
 # > AdGuard 解锁永久高级版
-^https?:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/AdGuardProCrack.js
+^https?:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt url script-response-body https://raw.githubusercontent.com/Jasonaax/Rewrite/master/UnlockVip/AdGuardProCrack.js
 
 [mitm] 
 hostname = *.adguard.org
